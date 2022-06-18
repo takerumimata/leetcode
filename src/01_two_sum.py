2471a8db4b51fad742d543d792cb8c6fd7beb17f
Takeru Mimata <takeru.mimata.dev@gmail.com>
2022-06-18 19:42:38 +0900

    solve 01 by python

---
+++ src/01_two_sum.py
@@ -0,0 +1,9 @@
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+    output = []
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            if nums[i] + nums[j] == target:
+                output.append(nums[i])
+                output.append(nums[j])
+                print(output)
+                return output
