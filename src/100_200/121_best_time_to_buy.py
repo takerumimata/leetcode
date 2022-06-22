d36f0d66cda7537a8757b8fb1ac526d97f480c11
Takeru Mimata <takeru.mimata.dev@gmail.com>
2022-06-23 00:30:47 +0900

    solve 121

---
+++ src/100_200/121_best_time_to_buy.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        """方針
+        下がり続けている間は見なくていい
+        """
+        max_prof = 0
+        start = 0
+        end = 1
+        while(end < len(prices)):
+            if(prices[start] >= prices[end]):
+                start = end
+                end += 1
+            else:
+                max_prof = max(max_prof, prices[end] - prices[start])
+                end += 1
+        return max_prof
