81f2c984b3d1c74a0ea25bf788c0d61cc19f893d
Takeru Mimata <takeru.mimata.dev@gmail.com>
2022-06-26 12:29:34 +0900

    solve by recursion function

--- src/200_300/226_invert_tree.py
+++ src/200_300/226_invert_tree.py
@@ -12,41 +12,52 @@ class TreeNode:
         self.right = right
 
 
+# class Solution:
+#     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+#         """invert binary tree
+#         2^(n-1)個のペアを反対から挿入していけばいい
+
+#         Args:
+#             root (Optional[TreeNode]): e.g.) [4,2,7,1,3,6,9]
+
+#         Returns:
+#             Optional[TreeNode]: e.g.) [4,7,2,9,6,3,1]
+#         """
+#         num_of_node = len(root)
+#         # 木の深さを求める
+#         depth = 0
+#         tmp = num_of_node
+#         while(True):
+#             if(tmp > 0):
+#                 tmp = tmp - 2**depth
+#                 depth += 1
+#             else:
+#                 break
+#         print(depth)
+#         ret = []
+#         inverted_range_prev = 0
+#         for i in range(depth):
+#             # 深さ^i-1 個ノードがあるので、そこの範囲を反転させていけばいい
+#             invert_length = 2 ** i
+#             inverted_range_cur = inverted_range_prev + invert_length
+#             inverted_range = root[inverted_range_prev:inverted_range_cur]
+#             inverted_range_prev = inverted_range_cur
+#             print(inverted_range)
+#             inverted_range.reverse()  # reverseの返り値はNone
+#             print(inverted_range)
+#             ret += inverted_range
+#         return ret
+
+
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        """invert binary tree
-        2^(n-1)個のペアを反対から挿入していけばいい
-
-        Args:
-            root (Optional[TreeNode]): e.g.) [4,2,7,1,3,6,9]
-
-        Returns:
-            Optional[TreeNode]: e.g.) [4,7,2,9,6,3,1]
-        """
-        num_of_node = len(root)
-        # 木の深さを求める
-        depth = 0
-        tmp = num_of_node
-        while(True):
-            if(tmp > 0):
-                tmp = tmp - 2**depth
-                depth += 1
-            else:
-                break
-        print(depth)
-        ret = []
-        inverted_range_prev = 0
-        for i in range(depth):
-            # 深さ^i-1 個ノードがあるので、そこの範囲を反転させていけばいい
-            invert_length = 2 ** i
-            inverted_range_cur = inverted_range_prev + invert_length
-            inverted_range = root[inverted_range_prev:inverted_range_cur]
-            inverted_range_prev = inverted_range_cur
-            print(inverted_range)
-            inverted_range.reverse()  # reverseの返り値はNone
-            print(inverted_range)
-            ret += inverted_range
-        return ret
+        if (root is None):
+            return root
+        if (root.right is not None or root.left is not None):
+            tmp = self.invertTree(root.right)
+            root.right = self.invertTree(root.left)
+            root.left = tmp
+        return root
 
 
 if __name__ == '__main__':
