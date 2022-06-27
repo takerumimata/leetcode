a890d65ace53f56aa8fccb2360f34c8e3ed93832
Takeru Mimata <takeru.mimata.dev@gmail.com>
2022-06-27 23:17:37 +0900

    solve

--- src/200_300/242_valid_anagram.py
+++ src/200_300/242_valid_anagram.py
@@ -1,23 +1,13 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        """まずは文字の長さが一致していること
+        """文字列をsortして、sortした結果が同じか判定する
         """
-        len_s = len(s)
-        len_t = len(t)
-        if(len_s != len_t):
-            print("length is not same")
-            return False
-        else:
-            for i in range(len_s):
-                rem_s = s[i]  # 取り除く文字
-                rem_t = t.find(rem_s)  # 文字があるか、あれば位置を返す
-                if(rem_t == -1):
-                    return False
-                else:
-                    t = list(t)
-                    t[rem_t] = ""
-                    t = "".join(t)
+        s = sorted(s)
+        t = sorted(t)
+        if(s == t):
             return True
+        else:
+            return False
 
 
 if __name__ == "__main__":
