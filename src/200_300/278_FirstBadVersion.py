c796520ee2cd45aa36a15e1211c9a3ba5bfc3504
Takeru Mimata <takeru.mimata.dev@gmail.com>
2022-07-13 00:08:29 +0900

    solve

--- src/200_300/278_FirstBadVersion.py
+++ src/200_300/278_FirstBadVersion.py
@@ -6,10 +6,20 @@ class Solution:
     def firstBadVersion(self, n: int) -> int:
         """二分探索
         """
-        left = 1
-        right = n
-        mid = (left + right) / 2
+        if(isBadVersion(1)):
+            return 1
+        left = 1  # not bad
+        right = n  # bad
+        mid = int((left + right) / 2)
         while(True):
-            if(isBadVersion(mid) and not (isBadVersion(mid + 1))):
-                return mid
+            if(not(isBadVersion(mid)) and isBadVersion(mid + 1)):
+                return int(mid + 1)
+            elif(isBadVersion(mid)):
+                # midがbad
+                right = mid
+                mid = int((left + right) / 2)  # 基本的に繰り下げ
+            else:
+                # midがnot bad
+                left = mid
+                mid = int((left + right) / 2)
         return 0
